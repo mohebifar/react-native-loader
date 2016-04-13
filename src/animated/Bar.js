636581f812d746352d08c2e3d3641336a737b794
mohebifar <netfars@gmail.com>
2016-04-13 17:48:45 +0430

    feat(Bar): Add `Bar` shape

---
+++ src/animated/Bar.js
@@ -0,0 +1,6 @@
+import { Animated } from 'react-native';
+import Bar from '../shapes/Bar';
+
+const AnimatedRectangle = Animated.createAnimatedComponent(Bar);
+
+export default AnimatedRectangle;
---
+++ src/shapes/Bar.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes, ART } from 'react-native';
+const { Shape, Path } = ART;
+
+export default class Bar extends Component {
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+  };
+
+  render() {
+    const { width, height } = this.props;
+
+    const path = Path()
+      .moveTo(width / -2, height * -1.5)
+      .line(0, height)
+      .line(width, 0)
+      .line(0, height)
+      .line(-width, 0)
+      .close();
+
+    return <Shape {...this.props} d={path}/>;
+  }
+}
