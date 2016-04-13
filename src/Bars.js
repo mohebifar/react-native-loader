7ddb2623afc4160a1fc112a84290a026afeb6416
mohebifar <netfars@gmail.com>
2016-04-13 17:50:55 +0430

    feat(Bars): Add Bars loader

---
+++ src/Bars.js
@@ -0,0 +1,93 @@
+import React, { Component, PropTypes, Animated, Easing, ART } from 'react-native';
+const { Surface } = ART;
+
+import Bar from './animated/Bar';
+
+export default class Bubbles extends Component {
+  static propTypes = {
+    size: PropTypes.number,
+    spaceBetween: PropTypes.number
+  };
+
+  state = {
+    bars: [
+      new Animated.Value(this.props.size),
+      new Animated.Value(this.props.size),
+      new Animated.Value(this.props.size),
+      new Animated.Value(this.props.size),
+      new Animated.Value(this.props.size)
+    ]
+  };
+
+  static defaultProps = {
+    spaceBetween: 4,
+    size: 20,
+    color: '#000'
+  };
+
+
+  componentDidMount() {
+    this.state.bars.forEach((val, index) => {
+      var timer = setTimeout(() => this.animate(index), index * 240);
+      this.timers.push(timer);
+    });
+  }
+
+  componentWillUnmount() {
+    this.timers.forEach(timer => clearTimeout(timer));
+    this.unmounted = true;
+  }
+
+  timers = [];
+
+  animate(index) {
+    Animated
+      .sequence([
+        Animated.timing(this.state.bars[index], {
+          toValue: this.props.size * 2.5,
+          duration: 600
+        }),
+        Animated.timing(this.state.bars[index], {
+          toValue: this.props.size,
+          duration: 600
+        })
+      ])
+      .start(() => {
+        if (!this.unmounted) {
+          this.animate(index);
+        }
+      });
+  }
+
+  renderBar(index) {
+    const { size, spaceBetween, color } = this.props;
+    const width = size / 3;
+    const x = width / 2 + (width + spaceBetween) * index;
+
+    return (<Bar
+      fill={color}
+      width={width}
+      height={this.state.bars[index]}
+      originY={0.5 * size}
+      originX={0.5}
+      y={size * 1.5}
+      x={x}
+    />);
+  }
+
+  render() {
+    const { size, spaceBetween } = this.props;
+    const width = size / 3 * 5 + spaceBetween * 4;
+    const height = size * 3;
+
+    return (<Surface
+      width={width}
+      height={height}>
+      {this.renderBar(0)}
+      {this.renderBar(1)}
+      {this.renderBar(2)}
+      {this.renderBar(3)}
+      {this.renderBar(4)}
+    </Surface>)
+  }
+}
