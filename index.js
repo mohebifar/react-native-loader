164f50a7cf48d04ab82260c1c8b3ec84d68a66b1
mohebifar <netfars@gmail.com>
2016-04-13 17:55:31 +0430

    chore(main): Add main file for package

---
+++ index.js
@@ -0,0 +1,9 @@
+import BubblesComponent from './src/Bubbles';
+import BarsComponent from './src/Bars';
+import PulseComponent from './src/Pulse';
+import DoubleBounceComponent from './src/DoubleBounce';
+
+export const Bubbles = BubblesComponent;
+export const Bars = BarsComponent;
+export const Pulse = PulseComponent;
+export const DoubleBounce = DoubleBounceComponent;
